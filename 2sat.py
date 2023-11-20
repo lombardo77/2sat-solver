b01237fbd3070f7dcb82a49622a6a6f82906b2aa
nick lombardo <lombardo.nicholas77@gmail.com>
2023-11-20 05:44:17 -0500

    we're satisfying formulas

--- 2sat.py
+++ 2sat.py
@@ -7,11 +7,8 @@ import sys
 def sat_solver(f):
     lst = verify(f)
     g = get_graph(lst)
-    print(lst)
-    for i in lst:
-        g[negate(i[0])].append(i[1])
-        g[negate(i[1])].append(i[0])
-    print(g)
+    gr = reverse_graph(g, lst)
+    kosaraju(g, gr)
 
 
 def negate(x):
@@ -21,7 +18,40 @@ def negate(x):
         return "~" + x[0]
 
 
-def get_graph(lst):
+def reverse_graph(g, lst):
+    gr = get_graph(lst, inhabit = False)
+    for v in g.keys():
+        for n in g[v]:
+            gr[n].append(v)
+    return gr
+
+
+def kosaraju(g, gr):
+    stack = []
+    visited = set()
+    scc = []
+
+    def dfs(g, n):
+        visited.add(n)
+        scc.append(n)
+        for i in g[n]:
+            if i not in visited:
+                dfs(g, i)
+        stack.append(n)
+
+    for v in g.keys():
+        if v not in visited:
+            dfs(g, v)
+    print("__________")
+    visited = set()
+    for v in stack[::-1]:
+        if v not in visited:
+            dfs(gr, v)
+            print(scc)
+            scc = []
+
+
+def get_graph(lst, inhabit=True):
     g = {}
     for k in lst:
         for i in k:
@@ -30,6 +60,12 @@ def get_graph(lst):
                 g[i[1]] = []
             else:
                 g["~" + i] = []
+    if not inhabit:
+        return g
+
+    for i in lst:
+        g[negate(i[0])].append(i[1])
+        g[negate(i[1])].append(i[0])
     return g
 
 
